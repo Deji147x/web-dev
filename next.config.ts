2b42c5e7f9ede01c956241e00619bf54628f7f11
Deji147x <101847955+Deji147x@users.noreply.github.com>
2025-12-31 20:34:01 -0500

    Configure Turbopack root in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   // @ts-ignore
   allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+  // @ts-ignore
+  turbopack: {
+    root: path.join(process.cwd(), '..'),
+  },
 };
 
 export default nextConfig;
